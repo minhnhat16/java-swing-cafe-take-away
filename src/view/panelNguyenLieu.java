7bad3f54cd0346c5e3fd7825b0c37d6c0de399ac
nghiapham0207 <nghiapham02072001@gmail.com>
2022-05-19 19:49:52 +0700

    test bỏ qua file

--- src/view/panelNguyenLieu.java
+++ src/view/panelNguyenLieu.java
@@ -153,10 +153,7 @@ public final class panelNguyenLieu extends javax.swing.JPanel {
                 ChinhsuaActionPerformed(evt);
             }
         });
-        gridBagConstraints = new java.awt.GridBagConstraints();
-        gridBagConstraints.anchor = java.awt.GridBagConstraints.BASELINE;
-        gridBagConstraints.insets = new java.awt.Insets(0, 10, 0, 10);
-        jPanel32.add(jButton16, gridBagConstraints);
+        jPanel32.add(jButton16, new java.awt.GridBagConstraints());
 
         Them.setIcon(new javax.swing.ImageIcon(getClass().getResource("/icon/floppy-disk-16.png"))); // NOI18N
         Them.setText("Thêm");
@@ -171,7 +168,7 @@ public final class panelNguyenLieu extends javax.swing.JPanel {
         buttonResetNL.setForeground(new java.awt.Color(0, 102, 204));
         buttonResetNL.setIcon(new javax.swing.ImageIcon(getClass().getResource("/icon/refresh-16.png"))); // NOI18N
         buttonResetNL.setText("Mới");
-        buttonResetNL.setPreferredSize(new java.awt.Dimension(80, 40));
+        buttonResetNL.setPreferredSize(new java.awt.Dimension(110, 35));
         buttonResetNL.addMouseListener(new java.awt.event.MouseAdapter() {
             public void mouseClicked(java.awt.event.MouseEvent evt) {
                 buttonResetNLMouseClicked(evt);
